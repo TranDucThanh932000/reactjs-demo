c0dd5087813c492b7160fb14802a712a6accb939
Tran Duc Thanh <thanh.td@vinayamato.com>
2023-02-22 00:01:19 +0700

    sidebar

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,9 +1,10 @@
 const routes = {
     home: '/',
-    folowing: '/following',
+    following: '/following',
     profile: '/@:nickname',
     upload: '/upload',
     search: '/search',
+    live: '/live',
 };
 
 export default routes;
